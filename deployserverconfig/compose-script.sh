dc2825cd6f13a546181d674b594a10f233ee8bde
Preethi Dovala <preethidovala@Preethis-MacBook-Pro.local>
2023-12-28 07:05:32 +0530

    php cicd

--- deployserverconfig/compose-script.sh
+++ deployserverconfig/compose-script.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 sudo DOCKER_PHP_IMAGE=$1 /usr/local/bin/docker-compose -f /home/ec2-user/deployserverconfig/docker-compose.yml up -d
+sudo cat /home/ec2-user/deployserverconfig/docker-compose.yml
